8056d99835baf1e3a79098a01c2d17df90afa79d
rtmoffat <rtmoffat@gmail.com>
2023-12-15 10:40:16 -0500

    Bug fixes per level, added flipper and bumper objects

--- Unity/PenBall/Assets/Scripts/GameController.cs
+++ Unity/PenBall/Assets/Scripts/GameController.cs
@@ -7,18 +7,27 @@ using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
-    public GameObject ball;
+    public GameObject ball=null;
     Rigidbody2D m_RigidBody2D;
     public Camera m_Camera;
+    
+    public GameObject leftFlipper_PF;
+    public GameObject rightFlipper_PF;
+    private float pivotOffset=10.0f;
+ 
     private Quaternion m_Camera_Rotation;
     // Start is called before the first frame update
     void Start()
     {
         //Kick the ball into motion
-        m_RigidBody2D = ball.GetComponent<Rigidbody2D>();
-        m_RigidBody2D.AddForce(m_RigidBody2D.transform.right*1000.0f,ForceMode2D.Impulse);
-        //Get initial camera rotation and don't change it. We only care about x/y axis.
-        m_Camera_Rotation=m_Camera.transform.rotation;
+        //only do this if the ball exists
+        if (ball)
+        {
+            m_RigidBody2D = ball.GetComponent<Rigidbody2D>();
+            m_RigidBody2D.AddForce(m_RigidBody2D.transform.right * 1000.0f, ForceMode2D.Impulse);
+            //Get initial camera rotation and don't change it. We only care about x/y axis.
+            m_Camera_Rotation = m_Camera.transform.rotation;
+        }
     }
 
     // Update is called once per frame
@@ -30,6 +39,12 @@ public class GameController : MonoBehaviour
         }
         //Keep camera initial rotation
         m_Camera.transform.rotation = m_Camera_Rotation;
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            print("Left flipper pressed");
+            Vector3 pivot = new Vector3();
+            pivot.z = 40.0f;
+            leftFlipper_PF.transform.Rotate(pivot, pivotOffset); 
+        }
     }
     private void LateUpdate()
     {
