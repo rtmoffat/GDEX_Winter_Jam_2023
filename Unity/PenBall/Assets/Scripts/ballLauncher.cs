ed4d76dfa0741b145c0c8955bd60c163742bfde4
rtmoffat <rtmoffat@gmail.com>
2023-12-25 22:08:57 -0500

    Added board and launch curves

--- Unity/PenBall/Assets/Scripts/ballLauncher.cs
+++ Unity/PenBall/Assets/Scripts/ballLauncher.cs
@@ -1,14 +1,20 @@
+using FMODUnity;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 
 public class ballLauncher : MonoBehaviour
 {
     public float launchForce=5000f;
     private bool launched = false;
+    private FMOD.Studio.EventInstance instance;
     // Start is called before the first frame update
     void Start()
     {
+        
+        
+        
         
     }
 
@@ -19,6 +25,9 @@ public class ballLauncher : MonoBehaviour
             print("Launching ball");
             launched = true;
             GetComponent<Rigidbody2D>().AddForce(new Vector2(0, launchForce));
+            FMODUnity.RuntimeManager.PlayOneShot("event:/SFX/Launcher");
+
+
         }
         //m_RigidBody2D = ball.GetComponent<Rigidbody2D>();
         //m_RigidBody2D.AddForce(m_RigidBody2D.transform.right * 1000.0f, ForceMode2D.Impulse);
